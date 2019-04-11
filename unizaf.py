254a630a01c373356025d3ef3e2fe12f4233d6e9
Zian25 <ziandv@gmail.com>
2019-04-11 17:48:15 -0300

    Update unizaf.py

--- unizaf.py
+++ unizaf.py
@@ -10,9 +10,9 @@ import platform
 while True:
 	os.system("clear")
 	print "Bem vindo ao Projeto-UniBrute-Force V1.2"
-	print  "Feito por Zian25 | Codinome Zawien-Fox"
-	print  "Selecione uma opção"
-	imprimir  "=========================="
+	print "Feito por Zian25 | Codinome Zawien-Fox"
+	print "Selecione uma opção"
+	print "=========================="
 	print "|   Brute-force FTP (1)  |"
 	print "|   Brute-force SMTP (2) |"
 	print "=========================="
